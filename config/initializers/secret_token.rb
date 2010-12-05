09fcc415a7343370d4df7a1a379df56a71958514
Shmangles <jessieangles@gmail.com>
2010-12-04 22:20:21 -0800

    Initial commit to clubflyers-com.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClubflyersCom::Application.config.secret_token = '037294278694bb8b9eb29bc787dfb7c1b08ca0a7427d7d4ce299c52be01d9dd41f212f0424ebdbc3e39710153b782073d4161d5d1299c096d7dd3f757764f3ff'
